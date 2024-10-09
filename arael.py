37a0f49b66eb727f506605266fa07c5ac3d85708
Spectre0ne <mmarquard737@gmail.com>
2024-10-09 21:42:46 +0200

    Added random ip generation and enabled command line args

--- arael.py
+++ arael.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from argparse import ArgumentParser
 
 from random import randint
 
@@ -8,6 +9,34 @@ def rand_ip():
     rand_ip = ".".join(map(str, ip))
     return(rand_ip)
 
-if __name__=="__main__":
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('-t', '--target', action='store', help='Specify the target IP address')
+    parser.add_argument('-p','--port', action='store', help='Specify the target port')
+    parser.add_argument('-c', '--count', action='store', help='Specify the amount of packets sent')
+    
+    errors = []
+
+    args = parser.parse_args()
+
+    if args.target and args.port and args.count: 
+        print("Arguments parsed")
+    else:
+        if args.target is None:
+            errors.append("Error: --t or --target is required.")
+        if args.port is None:
+            errors.append("Error: --p or --port is required.")
+        if args.count is None:
+            errors.append("Error: --c or --count is required.")
+    if errors:
+        raise ValueError("\n".join(errors))
+
+if __name__ == "__main__":
+    main()
     print(rand_ip())
+
+
+
 
